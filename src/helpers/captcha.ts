2983a9e47f08bc2e8276cd710f09fb5736f1c83f
Martin <emtorres@opensuse.org>
2020-10-20 10:10:09 -0300

    change configuration captcha again

--- src/helpers/captcha.ts
+++ src/helpers/captcha.ts
@@ -2,11 +2,11 @@ import { create } from 'svg-captcha'
 import { convert } from 'convert-svg-to-png'
 
 export async function getImageCaptcha() {
-  //const letters = 'abcdefghijklmnopqrstuvwxyz'
+  const letters = 'abcdefghijklmnopqrstuvwxyz'
   //const letters = 'osl'
   const catpcha = create({
     size: 4,
-    //ignoreChars: letters + letters.toUpperCase(),
+    ignoreChars: letters + letters.toUpperCase(),
     noise: 3,
     width: 170,
     height: 100,
